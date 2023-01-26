2e1e4ffb7c421ee29f0cd3c36114ca5f112d3677
andresbertone <andresbertone97@gmail.com>
2023-01-26 18:44:37 -0300

    Feat: Eliminar repuesto

--- frontend/src/app/core/services/spare-part.service.ts
+++ frontend/src/app/core/services/spare-part.service.ts
@@ -4,18 +4,23 @@ import { Observable } from 'rxjs';
 import { HttpService } from './common/http.service';
 
 import { environment } from 'src/environments/environment';
-import { SparePartsModel } from './../models/spare-part/spare-parts.model';
+import { SparePartsModel } from '../models/spare-part/spare-parts.model';
+import { SparePartModel } from '../models/spare-part/spare-part.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SparePartService {
 
-  readonly baseUrl: string = environment.baseUrl;
+  readonly baseUrl: string = environment.baseUrl + '/sparePart';
 
   constructor(private httpService: HttpService) { }
 
   getSpareParts(): Observable<SparePartsModel> {
-    return this.httpService.httpGetAll(`${this.baseUrl}/sparePart`);
+    return this.httpService.httpGetAll(`${this.baseUrl}`);
+  }
+
+  deleteSparePart(sparePartId: number): Observable<SparePartModel> {
+    return this.httpService.httpDelete(`${this.baseUrl}/${sparePartId}`);
   }
 }
--- frontend/src/app/modules/spare-part/components/spare-parts/spare-parts.component.ts
+++ frontend/src/app/modules/spare-part/components/spare-parts/spare-parts.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { SparePartService } from 'src/app/core/services/spare-part.service';
 import { SpinnerService } from 'src/app/core/services/common/spinner.service';
+import { DialogService } from 'src/app/core/services/common/dialog.service';
+import { AlertService } from 'src/app/core/services/common/alert.service';
 
 import { SparePartsModel } from 'src/app/core/models/spare-part/spare-parts.model';
 import { SparePartModel } from 'src/app/core/models/spare-part/spare-part.model';
@@ -22,11 +24,17 @@ export class SparePartsComponent implements OnInit {
   displayedColumns: string[] = ['SparePartCode', 'SparePartDescription', 'SparePartPrice', 'Stock', 'SparePartSupplier', 'Action'];
   paginator!: MatPaginator;
 
+  LOW_STOCK: number = 5;
+
   @ViewChild(MatPaginator) set matPaginator(matPaginator: MatPaginator) {
     this.initializePaginator(matPaginator);
   }
 
-  constructor(private sparePartService: SparePartService, private spinnerService: SpinnerService) {
+  constructor(
+    private sparePartService: SparePartService, 
+    private spinnerService: SpinnerService,
+    private dialogService: DialogService,
+    private alertService: AlertService) {
     this.spareParts = new MatTableDataSource();
   }
 
@@ -65,6 +73,25 @@ export class SparePartsComponent implements OnInit {
   }
 
   isLowStock(sparePartStock: number) {
-    return sparePartStock <= 5;
+    return sparePartStock <= this.LOW_STOCK;
+  }
+
+  deleteSparePart(sparePart: SparePartModel) {
+    this.dialogService.showWarning(
+      'Delete spare part',
+      `Are you sure you want to delete the spare part ${sparePart.sparePartDescription}?`,
+      'No',
+      'Delete',
+      true
+    ).afterClosed().subscribe((result) => {
+      if (result) {
+        this.sparePartService.deleteSparePart(sparePart.sparePartId).subscribe(
+          (sparePart: SparePartModel) => {
+            this.alertService.openSnackBar(`The spare part ${sparePart.sparePartDescription} was successfully deleted.`);
+            this.loadSpareParts();
+          }
+        );
+      }
+    });
   }
 }
